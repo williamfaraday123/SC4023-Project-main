5ae756041912eb181bffa8ea7c97ac148178a0de
tttennnnn <ttenn.sapol@gmail.com>
2026-03-30 12:14:42 +0800

    Remove old flat file structure and unused files

--- Enum/Metrics.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Metrics(Enum):
-    """Enumeration for the different metrics computed from resale prices data."""
-    MIN_PRICE = 'Minimum Price'
-    AVG_PRICE = 'Average Price'
-    STDDEV = 'Standard Deviation of Price'
-    MIN_PRICE_PER_SQM = 'Minimum Price per Square Meter'
--- tests/README.md
+++
@@ -1,8 +0,0 @@
-Requires Docker, uses Postgres
-
-From root:
-
-```pip install tests/requirements.txt```
-
-```pytest -v -s tests/test_result.py```
-
